07be586d051105068e2dd0724da5fc788ecfb3cb
albertuchr <albertuschristianto27@gmail.com>
2025-09-16 21:17:03 +0700

    Commit No.4 – use jetpack navigation

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -16,18 +16,13 @@ class MainActivity : AppCompatActivity(), CoffeeListener {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.
+        fragment_container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.
+            systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right,
+                systemBars.bottom)
             insets
-        }
-
-        if (savedInstanceState == null) {
-            val listFragment = ListFragment()
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, listFragment)
-                .commit()
         }
     }
 
